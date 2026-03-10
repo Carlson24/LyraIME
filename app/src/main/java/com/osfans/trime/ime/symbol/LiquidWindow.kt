6b58c7307d56ba9f4e35bf7602de1475defa8768
iSilen <lzlv312@gmail.com>
2026-03-11 00:23:39 +0800

    feat(liquid): 液态键盘添加VAR_LENGTH类型，丰富操作按钮样式
    - 修复keyHeight定义了却未使用
    - "常用"和"更多"列表改为使用VAR_LENGTH布局
    - 按钮支持独立属性：click、label、width、height、margin、padding
      边距格式：10(四周)、[四周]、[左右, 上下]、[左, 上, 右, 下]

--- app/src/main/java/com/osfans/trime/ime/symbol/LiquidWindow.kt
+++ app/src/main/java/com/osfans/trime/ime/symbol/LiquidWindow.kt
@@ -10,6 +10,7 @@ import androidx.core.content.ContextCompat
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import com.osfans.trime.daemon.RimeSession
 import com.osfans.trime.daemon.launchOnReady
 import com.osfans.trime.data.SymbolHistory
@@ -22,6 +23,7 @@ import com.osfans.trime.ime.window.BoardWindow
 import com.osfans.trime.ime.window.BoardWindowManager
 import com.osfans.trime.ime.window.ResidentWindow
 import org.kodein.di.instance
+import splitties.dimensions.dp
 
 class LiquidWindow :
     BoardWindow.BarBoardWindow(),
@@ -38,11 +40,23 @@ class LiquidWindow :
     private val symbolHistory = SymbolHistory(180)
     var currentDataType: LiquidData.Type = LiquidData.Type.SINGLE
         private set
+    private var currentTabIndex: Int = -1
 
     private val adapter by lazy {
         LiquidAdapter(theme) {
             when (currentDataType) {
-                LiquidData.Type.SYMBOL -> triggerSymbolInput(this.altText)
+                LiquidData.Type.SYMBOL -> triggerSymbolInput(this.text)
+                LiquidData.Type.VAR_LENGTH -> {
+                    // VAR_LENGTH类型：如果altText和text不同，说明是键值对，像SYMBOL一样处理
+                    // 如果altText和text相同，说明是纯文本，像SINGLE一样直接上屏
+                    if (this.altText != this.text) {
+                        triggerSymbolInput(this.text)
+                    } else {
+                        service.commitText(this.text)
+                        symbolHistory.insert(this.text)
+                        symbolHistory.save()
+                    }
+                }
                 LiquidData.Type.TABS -> {
                     val realPosition = LiquidData.getTagList()
                         .indexOfFirst { it.label == this.text }
@@ -92,23 +106,76 @@ class LiquidWindow :
     override fun onDetached() {}
 
     fun setDataByIndex(i: Int) {
+        if (currentTabIndex == i) {
+            return
+        }
+        currentTabIndex = i
+
         val tag = LiquidData.getTagList()[i]
         currentDataType = tag.type
         liquidLayout.tabsUi.activateTab(i)
-        when (tag.type) {
+
+        // 更新 adapter 的数据类型
+        val dataTypeChanged = adapter.currentDataType != currentDataType
+        adapter.currentDataType = currentDataType
+
+        // 根据数据类型动态设置 LayoutManager 的 justifyContent
+        mainLayoutManager.justifyContent =
+            if (LiquidData.isVarLengthType(currentDataType)) {
+                JustifyContent.SPACE_BETWEEN
+            } else {
+                JustifyContent.FLEX_START
+            }
+
+        // 数据类型改变时，先通知刷新以重新创建 ViewHolder，然后再提交数据
+        if (dataTypeChanged) adapter.notifyDataSetChanged()
+
+        val data = when (tag.type) {
             LiquidData.Type.HISTORY -> {
                 symbolHistory.load()
-                submitData(symbolHistory.toOrderedList().map { LiquidKeyboard.KeyItem(it) })
+                symbolHistory.toOrderedList().map { LiquidKeyboard.KeyItem(it) }
             }
             else -> {
-                val data = LiquidData.getDataByIndex(i)
-                submitData(data)
+                LiquidData.getDataByIndex(i)
             }
+        }
+
+        if (LiquidData.isVarLengthType(currentDataType)) {
+            val originalData = data
+            val expectedTabIndex = currentTabIndex
+            val expectedDataType = currentDataType
+
+            liquidLayout.recyclerView.post {
+                if (currentTabIndex != expectedTabIndex || currentDataType != expectedDataType) {
+                    return@post
+                }
+                val dataWithPlaceholders = originalData.toMutableList()
+                val placeholderCount = calculatePlaceholderCount()
+                repeat(placeholderCount) {
+                    dataWithPlaceholders.add(LiquidKeyboard.KeyItem("", ""))
+                }
+                submitData(dataWithPlaceholders)
+            }
+        } else {
+            submitData(data)
         }
     }
 
     private fun submitData(data: List<LiquidKeyboard.KeyItem>) {
         adapter.submitList(data)
+    }
+
+    private fun calculatePlaceholderCount(): Int {
+        val recyclerView = liquidLayout.recyclerView
+        val containerWidth = recyclerView.width
+        if (containerWidth <= 0) return 0
+
+        val singleWidth = context.dp(theme.liquidKeyboard.singleWidth)
+        val marginX = context.dp(theme.liquidKeyboard.marginX).toInt()
+        val itemTotalWidth = singleWidth + 2 * marginX
+        if (itemTotalWidth <= 0) return 0
+
+        return containerWidth / itemTotalWidth
     }
 
     private fun triggerSymbolInput(symbol: String) {
