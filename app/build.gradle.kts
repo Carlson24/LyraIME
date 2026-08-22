fd12f32385faa4788020d8e064205362a55cec8d
Carlson24 <aubreycarlson24@outlook.com>
2026-08-22 17:01:05 +0800

    update 5.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 28
         targetSdk = 37
         versionCode = LocalDate.now(ZoneId.of("Asia/Shanghai")).format(DateTimeFormatter.BASIC_ISO_DATE).toInt()
-        versionName = "5.4.0"
+        versionName = "5.4.5"
 
         multiDexEnabled = true
         buildConfigField("String", "BUILDER", "\"${project.builder}\"")
